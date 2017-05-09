f3fe84c7a48694d30ef723a522df26ff6dde9217
zhaoliang <2668645098@qq.com>
2017-05-09 09:54:25 +0800

    初始化项目

---
+++ app/src/main/java/zhaoliang/com/hmandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package zhaoliang.com.hmandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
